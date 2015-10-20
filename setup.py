f5e3e44ab084de253a434fe2e596e1566d0f0efe
Austin Rose <austin@percipientnetworks.com>
2015-10-20 13:02:41 -0400

    Clean up packaging files.

--- setup.py
+++ setup.py
@@ -3,17 +3,18 @@ from setuptools import setup, find_packages
 setup(
     name="django-allauth-2fa",
     version="0.1",
-    packages=find_packages(exclude=['testproject']),
+    packages=find_packages(),
     install_requires=[
-        package for package in open('requirements.txt', 'r').readlines()
+        "django>=1.7.0",
+        "qrcode==5.1",
+        "django-allauth==0.23.0",
+        "django-otp==0.3.1",
     ],
-
     author="Víðir Valberg Guðmundsson",
     author_email="valberg@orn.li",
     description="Adds two factor authentication to django-allauth",
     license="PSF",
     keywords="otp auth two factor authentication allauth django",
-    url="https://github.com/valberg/django-allauth-2fa",
-
+    url="https://github.com/percipient/django-allauth-2fa",
     long_description=open('README.rst', 'r').read(),
 )
