3ac0933c9022df9d80d19ebc20e27e65d8f19afb
Austin Rose <austin@percipientnetworks.com>
2015-10-20 12:49:53 -0400

    Minor style changes to urls.py

--- allauth_2fa/urls.py
+++ allauth_2fa/urls.py
@@ -1,29 +1,24 @@
+from django.conf.urls import url, patterns
+
 from . import views
-from django.conf.urls import patterns, url
 
 
-urlpatterns = patterns(
-    '',
-    url(
-        r"^two-factor-authenticate$",
+urls =  [
+    url(r"^two-factor-authenticate$",
         views.two_factor_authenticate,
-        name="two-factor-authenticate"
-    ),
+        name="two-factor-authenticate"),
 
-    url(
-        r"^two_factor/setup$",
+    url(r"^two_factor/setup$",
         views.two_factor_setup,
-        name="two-factor-setup"
-    ),
+        name="two-factor-setup"),
 
-    url(
-        r"^two_factor/backup_tokens$",
+    url(r"^two_factor/backup_tokens$",
         views.two_factor_backup_tokens,
-        name="two-factor-backup-tokens"
-    ),
-    url(
-        r"^two_factor/qr_code$",
+        name="two-factor-backup-tokens"),
+
+    url(r"^two_factor/qr_code$",
         views.two_factor_qr_code_generator,
-        name="two-factor-qr-code"
-    ),
-)
+        name="two-factor-qr-code"),
+]
+
+urlpatterns = patterns('', *urls)
