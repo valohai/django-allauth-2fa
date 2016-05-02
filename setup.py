b78188f2ae0e4e1fc372e2c149f9798733c20d90
Patrick Cloke <clokep@patrick.cloke.us>
2016-05-02 09:14:52 -0400

    Bump version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-allauth-2fa",
-    version="0.1.3",
+    version="0.1.4",
     packages=find_packages(),
     install_requires=[
         "django>=1.7.0",
