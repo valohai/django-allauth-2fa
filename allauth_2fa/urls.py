0102052447119ba933a450241e9a90887a5f8555
stvnrlly <stvnrlly@stvnrlly.com>
2017-02-11 00:25:40 -0500

    make slash optional

--- allauth_2fa/urls.py
+++ allauth_2fa/urls.py
@@ -4,23 +4,23 @@ from allauth_2fa import views
 
 
 urlpatterns = [
-    url(r"^two-factor-authenticate/$",
+    url(r"^two-factor-authenticate/?$",
         views.TwoFactorAuthenticate.as_view(),
         name="two-factor-authenticate"),
 
-    url(r"^two_factor/setup/$",
+    url(r"^two_factor/setup/?$",
         views.TwoFactorSetup.as_view(),
         name="two-factor-setup"),
 
-    url(r"^two_factor/backup_tokens/$",
+    url(r"^two_factor/backup_tokens/?$",
         views.TwoFactorBackupTokens.as_view(),
         name="two-factor-backup-tokens"),
 
-    url(r"^two_factor/qr_code/$",
+    url(r"^two_factor/qr_code/?$",
         views.QRCodeGeneratorView.as_view(),
         name="two-factor-qr-code"),
 
-    url(r"^two_factor/remove/$",
+    url(r"^two_factor/remove/?$",
         views.TwoFactorRemove.as_view(),
         name="two-factor-remove"),
 ]
