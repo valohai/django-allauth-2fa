6e32196013545420f67667f165c5489bfa9adf3e
Patrick Cloke <clokep@patrick.cloke.us>
2017-02-17 10:29:18 -0500

    Add a test-case.

--- tests/test_allauth_2fa.py
+++ tests/test_allauth_2fa.py
@@ -221,6 +221,34 @@ class Test2Factor(TestCase):
                              reverse('account_login'),
                              fetch_redirect_response=False)
 
+    def test_backwards_compatible_url(self):
+        """Ensure that the old 2FA URLs still work."""
+        user = get_user_model().objects.create(username='john')
+        user.set_password('doe')
+        user.save()
+        totp_model = user.totpdevice_set.create()
+
+        resp = self.client.post(reverse('account_login'),
+                                {'login': 'john',
+                                 'password': 'doe'})
+        self.assertRedirects(resp,
+                             reverse('two-factor-authenticate'),
+                             fetch_redirect_response=False)
+
+        # Now ensure that logging in actually works.
+        totp = TOTP(totp_model.bin_key, totp_model.step, totp_model.t0, totp_model.digits)
+
+        # The old URL doesn't have a trailing slash.
+        url = reverse('two-factor-authenticate').rstrip('/')
+
+        resp = self.client.post(url, {'otp_token': totp.token()})
+        self.assertRedirects(resp,
+                             settings.LOGIN_REDIRECT_URL,
+                             fetch_redirect_response=False)
+
+        # Ensure the signal is received as expected.
+        self.assertEqual(self.user_logged_in_count, 1)
+
 
 @unittest.skipIf(not MiddlewareMixin, 'Additional middleware tests are Django > 1.10.')
 @override_settings(MIDDLEWARE=settings.MIDDLEWARE_CLASSES, MIDDLEWARE_CLASSES=[])
