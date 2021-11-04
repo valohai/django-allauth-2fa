26b210d92610d25e0b7801128970d0a1417ae625
Víðir Valberg Guðmundsson <valberg@orn.li>
2021-11-04 23:15:39 +0100

    Fix urls so trailing slashes are optional again.

--- allauth_2fa/urls.py
+++ allauth_2fa/urls.py
@@ -1,21 +1,21 @@
-from django.urls import path
+from django.urls import re_path
 
 from allauth_2fa import views
 
 urlpatterns = [
-    path("two-factor-authenticate/",
+    re_path("^two-factor-authenticate/?",
         views.TwoFactorAuthenticate.as_view(),
         name="two-factor-authenticate"),
 
-    path("two_factor/setup/",
+    re_path("^two_factor/setup/?",
         views.TwoFactorSetup.as_view(),
         name="two-factor-setup"),
 
-    path("two_factor/backup_tokens/",
+    re_path("^two_factor/backup_tokens/?",
         views.TwoFactorBackupTokens.as_view(),
         name="two-factor-backup-tokens"),
 
-    path("two_factor/remove/",
+    re_path("^two_factor/remove/?",
         views.TwoFactorRemove.as_view(),
         name="two-factor-remove"),
 ]
