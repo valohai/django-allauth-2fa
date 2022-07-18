2d38bc944e577b864713e3ad91b68c90b38a1a9e
Aarni Koskela <akx@iki.fi>
2022-07-18 08:24:23 +0300

    Don't try to interrogate function-based views for `next`
    Fixes #143
    
    Augments #99

--- allauth_2fa/adapter.py
+++ allauth_2fa/adapter.py
@@ -19,9 +19,21 @@ class OTPAdapter(DefaultAccountAdapter):
             # Cast to string for the case when this is not a JSON serializable
             # object, e.g. a UUID.
             request.session["allauth_2fa_user_id"] = str(user.id)
+            redirect_url = self.get_2fa_authenticate_url(request)
+            raise ImmediateHttpResponse(response=HttpResponseRedirect(redirect_url))
 
-            redirect_url = reverse("two-factor-authenticate")
-            # Add "next" parameter to the URL.
+        # Otherwise defer to the original allauth adapter.
+        return super().login(request, user)
+
+    def get_2fa_authenticate_url(self, request):
+        """
+        Get the URL to redirect to for finishing 2FA authentication.
+        """
+        redirect_url = reverse("two-factor-authenticate")
+
+        # Add "next" parameter to the URL if possible.
+        # If the view function smells like a class-based view, we can interrogate it.
+        if getattr(request.resolver_match.func, "view_class", None):
             view = request.resolver_match.func.view_class()
             view.request = request
             success_url = view.get_success_url()
@@ -31,7 +43,4 @@ class OTPAdapter(DefaultAccountAdapter):
             if query_params:
                 redirect_url += "?" + urlencode(query_params)
 
-            raise ImmediateHttpResponse(response=HttpResponseRedirect(redirect_url))
-
-        # Otherwise defer to the original allauth adapter.
-        return super().login(request, user)
+        return redirect_url
