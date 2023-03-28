eb56b7204fb284c7a799c5ed79339fe941c05b9b
Aarni Koskela <akx@iki.fi>
2023-03-28 15:02:04 +0300

    Add some type annotations

--- allauth_2fa/adapter.py
+++ allauth_2fa/adapter.py
@@ -6,18 +6,23 @@ from allauth.account.adapter import DefaultAccountAdapter
 from allauth.exceptions import ImmediateHttpResponse
 from allauth.socialaccount.models import SocialLogin
 from django.core.exceptions import PermissionDenied
-from django.http import HttpResponseRedirect
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 
 from allauth_2fa.utils import user_has_valid_totp_device
 
 
 class OTPAdapter(DefaultAccountAdapter):
-    def has_2fa_enabled(self, user):
+    def has_2fa_enabled(self, user) -> bool:
         """Returns True if the user has 2FA configured."""
         return user_has_valid_totp_device(user)
 
-    def pre_login(self, request, user, **kwargs):
+    def pre_login(
+        self,
+        request: HttpRequest,
+        user,
+        **kwargs,
+    ) -> HttpResponse | None:
         response = super().pre_login(request, user, **kwargs)
         if response:
             return response
@@ -31,7 +36,7 @@ class OTPAdapter(DefaultAccountAdapter):
         # Otherwise defer to the original allauth adapter.
         return super().login(request, user)
 
-    def get_2fa_authenticate_url(self, request):
+    def get_2fa_authenticate_url(self, request: HttpRequest) -> str:
         """
         Get the URL to redirect to for finishing 2FA authentication.
         """
@@ -51,7 +56,12 @@ class OTPAdapter(DefaultAccountAdapter):
 
         return redirect_url
 
-    def stash_pending_login(self, request, user, login_kwargs):
+    def stash_pending_login(
+        self,
+        request: HttpRequest,
+        user,
+        login_kwargs: dict,
+    ) -> None:
         """Here, we're going to stash the pending login so that it can be
         resumed at a later point. The `login_kwargs` contain meta information on
         the login which we need to store.
@@ -76,7 +86,7 @@ class OTPAdapter(DefaultAccountAdapter):
                 login_kwargs["signal_kwargs"] = signal_kwargs
         request.session["allauth_2fa_login"] = login_kwargs
 
-    def unstash_pending_login_kwargs(self, request):
+    def unstash_pending_login_kwargs(self, request: HttpRequest) -> dict:
         login_kwargs = request.session.pop("allauth_2fa_login", None)
         if login_kwargs is None:
             raise PermissionDenied()
--- allauth_2fa/mixins.py
+++ allauth_2fa/mixins.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from django.contrib.auth.mixins import AccessMixin
-from django.http import HttpResponseRedirect
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.urls import reverse_lazy
 
 from allauth_2fa.utils import user_has_valid_totp_device
@@ -10,12 +10,12 @@ from allauth_2fa.utils import user_has_valid_totp_device
 class ValidTOTPDeviceRequiredMixin(AccessMixin):
     no_valid_totp_device_url = reverse_lazy("two-factor-setup")
 
-    def dispatch(self, request, *args, **kwargs):
+    def dispatch(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
         if not request.user.is_authenticated:
             return self.handle_no_permission()
         if not user_has_valid_totp_device(request.user):
             return self.handle_missing_totp_device()
         return super().dispatch(request, *args, **kwargs)
 
-    def handle_missing_totp_device(self):
+    def handle_missing_totp_device(self) -> HttpResponse:
         return HttpResponseRedirect(self.no_valid_totp_device_url)
