3d69e757bc61c30f1faeb82c8e5af34d0eebe238
Aarni Koskela <akx@iki.fi>
2023-07-04 14:59:05 +0300

    Upgrade Ruff

--- tests/urls.py
+++ tests/urls.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-from django.urls import include
-from django.urls import path
+from django.urls import include, path
 
 
 def blank_view(request):
