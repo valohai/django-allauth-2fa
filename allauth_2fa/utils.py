919ca157aa6d8829471d6f61337b956c7e7bcbb9
Preston Badeer <467756+pbadeer@users.noreply.github.com>
2023-09-22 08:14:44 -0500

    Update utils.py
    Add get_next_query_string util from @akx

--- allauth_2fa/utils.py
+++ allauth_2fa/utils.py
@@ -35,3 +35,27 @@ def user_has_valid_totp_device(user) -> bool:
     if not user.is_authenticated:
         return False
     return user.totpdevice_set.filter(confirmed=True).exists()
+
+
+def get_next_query_string(request: HttpRequest) -> str | None:
+    """
+    Get the query string (including the prefix `?`) to redirect to after a successful POST.
+    
+    If a query string can't be determined, returns None.
+    """
+    # If the view function smells like a class-based view, we can interrogate it.
+    try:
+        view = request.resolver_match.func.view_class()
+        redirect_field_name = view.redirect_field_name
+    except AttributeError:
+        # Interrogation failed :(
+        return None
+
+    view.request = request
+    query_params = request.GET.copy()
+    success_url = view.get_success_url()
+    if success_url:
+        query_params[redirect_field_name] = success_url
+    if query_params:
+        return f"?{urlencode(query_params)}"
+    return None
